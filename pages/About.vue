96a2a52ca8c7bdfba489037be8d477094f60a91e
omemoji <omemoji1111@gmail.com>
2022-12-10 19:15:02 +0900

    add README(omemoji)

--- pages/About.vue
+++ pages/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="card card-shadow p-10 m-3">
-    <ContentDoc path="/about" class="markdown-body" />
+    <ContentDoc path="/about/readme" class="markdown-body" />
   </div>
 </template>
