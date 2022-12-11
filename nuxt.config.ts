6c923f3726ba2dbe97c4b4e9c1c9454e5098d9ee
omemoji <omemoji1111@gmail.com>
2022-12-11 19:01:37 +0900

    change to custom domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: "/omemoji.com/",
+    baseURL: "/",
     head: {
   
       titleTemplate: "%s | 創作物紹介",
