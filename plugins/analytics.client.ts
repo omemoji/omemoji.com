7a922657391f2d3a092f5559c2802956de385ca1
omemoji <omemoji1111@gmail.com>
2022-12-12 16:21:56 +0900

    defer Google Analytics

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -10,6 +10,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(
     VueGtag,
     {
+      deferScriptLoad: true,
       appName: '創作物紹介',                 // サイトの名称
       pageTrackerScreenviewEnabled: true, // ページトラッキングスクリーンビューを有効
       config: { id: `G-16EGCD4LQV` },     // GoogleAnalytics(GA4)の測定IDを指定する
