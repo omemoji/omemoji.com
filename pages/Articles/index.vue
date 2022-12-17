267559e0eededbd1d60059605f6ff567fa244677
omemoji <omemoji1111@gmail.com>
2022-12-17 21:56:05 +0900

    注意書きを英語にする

--- pages/Articles/index.vue
+++ pages/Articles/index.vue
@@ -21,7 +21,7 @@
       >/<nuxt-link to="/articles" class="color-md">articles</nuxt-link>
     </p>
     <h1 class="text-center">Articles</h1>
-    <p class="text-center mt-3">記事は予告なく変更される可能性がある。</p>
+    <p class="text-center mt-3">This articles are subject to change without notice. </p>
     <main>
       <ContentList path="/articles">
         <div
