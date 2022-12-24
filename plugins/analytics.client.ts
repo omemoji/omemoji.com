d16f5ff2becdb5c13b2350c1ac65e75f4cb7c492
omemoji <68148226+omemoji@users.noreply.github.com>
2022-12-24 22:53:03 +0900

    pageTrackerScreenviewEnabled: true

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -11,6 +11,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     {
       deferScriptLoad: true,
       appName: "創作物紹介", // サイトの名称
+      pageTrackerScreenviewEnabled: true, // ページトラッキングスクリーンビューを有効
       config: { id: `G-16EGCD4LQV` }, // GoogleAnalytics(GA4)の測定IDを指定する
     },
     router
