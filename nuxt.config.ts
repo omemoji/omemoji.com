184398a7257b7b2749f547f2c33076e822d4bb90
omemoji <68148226+omemoji@users.noreply.github.com>
2022-12-25 14:21:49 +0900

    payloadExtraction: true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  experimental: {
-    payloadExtraction: false,
-  },
+  // experimental: {
+  //   payloadExtraction: false,
+  // },
   app: {
     baseURL: "/",
     head: {
