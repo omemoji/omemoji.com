cbff5d005883b6f97d93830b738e5df27b60a547
omemoji <68148226+omemoji@users.noreply.github.com>
2022-12-31 23:31:37 +0900

    add 2023

---
+++ components/global/article-image.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt-img
+    provider="ipx_fixed"
+    quality="40"
+    :width="width"
+    :height="height"
+    :src="src"
+    class="mb-4"
+    format="webp"
+  ></nuxt-img>
+</template>
+<script>
+export default {
+  props: ["width", "height", "src"],
+};
+</script>
