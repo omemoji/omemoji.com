d536cdcbc7800f0981d3d45c1bdf2ed5355c82d5
omemoji <68148226+omemoji@users.noreply.github.com>
2023-12-03 17:21:07 +0900

    Footerの年表記をやめる

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,8 @@
-import Year from "components/Year";
 import LinkList from "components/LinkList";
 export default function Footer() {
   return (
     <footer className="h-24 flex px-4 border-t">
-      <div className="my-auto">
-        &copy; <Year /> omemoji
-      </div>
+      <div className="my-auto">&copy; omemoji</div>
       <LinkList className="my-auto ml-auto" />
     </footer>
   );
