fd4afd11482bfb6393d190d12dda023dc9cd9723
omemoji <68148226+omemoji@users.noreply.github.com>
2023-12-06 00:39:46 +0900

    add site-map exclusion

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,12 @@ const config = {
   siteUrl: process.env.SITE_URL || "https://omemoji.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: [
+    "/artworks/tag/*",
+    "/artworks/page/*",
+    "/articles/tag/*",
+    "/articles/page/*",
+  ],
   outDir: "./out",
 };
 
