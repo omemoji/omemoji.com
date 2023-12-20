0f342fbadaaff5a3fd25e32fcf489efe0ce143b4
omemoji <me@omemoji.com>
2023-12-21 00:33:40 +0900

    [update]記事の個別ページのタグが折り返すようにする

--- src/components/TopArticle/index.tsx
+++ src/components/TopArticle/index.tsx
@@ -9,19 +9,17 @@ export type Props = {
 
 export default async function TopArticle({ emoji, title, tags, date }: Props) {
   return (
-    <div className="top-article mb-8">
-      <p className="mb-4 mx-auto emoji text-center leading-relaxed">{emoji}</p>
+    <div className="top-article mb-8 text-center">
+      <p className="mb-4 mx-auto emoji leading-relaxed">{emoji}</p>
       <h1 className="text-2xl xs:text-3xl mt-8 mb-2">{title}</h1>
-      <div className="flex justify-center mb-4 ">
+      <div className="inline-block  mb-4 ">
         {tags.map((tag) => (
-          <p key={tag}>
-            <Link href={"/articles/tag/" + tag} className="tag text-xl">
-              {"#" + tag}
-            </Link>
-          </p>
+          <Link key={tag} href={"/articles/tag/" + tag} className="tag text-xl">
+            {"#" + tag}
+          </Link>
         ))}
       </div>
-      <p className="text-center text-xl mb-8">Published: {date}</p>
+      <p className="text-xl mb-8">Published: {date}</p>
     </div>
   );
 }
