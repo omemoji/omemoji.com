4f14d0281536fd40a22f38bb382267ac21f5921d
omemoji <me@omemoji.com>
2025-01-19 02:20:58 +0900

    [fix]delete trailing slash

--- astro.config.ts
+++ astro.config.ts
@@ -83,6 +83,7 @@ export default defineConfig({
     remotePatterns: [{ protocol: "https" }],
   },
   site: "https://omemoji.com/",
+  trailingSlash: "never",
   experimental: {
     svg: {
       mode: "inline",
