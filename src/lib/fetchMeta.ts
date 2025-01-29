b4dae9617d5ddced503c23beb4daa4a45468fa94
omemoji <me@omemoji.com>
2025-01-29 21:37:57 +0900

    [update]fetch Metadata

--- src/lib/fetchMeta.ts
+++ src/lib/fetchMeta.ts
@@ -1,6 +1,37 @@
 import { load } from "cheerio";
 import type { CheerioAPI } from "cheerio";
-import getImageSize from "@/lib/getImageSize";
+import { getPlaiceholder } from "plaiceholder";
+
+interface Metadata {
+  url: string;
+  title: string;
+  description: string;
+  image: { url: string; width: number; height: number };
+}
+
+const getImageMeta = async (src: string) => {
+  if (src === "" || src === undefined) {
+    return { img: { url: "", width: 0, height: 0 } };
+  } else {
+    const buffer: Buffer = await fetch(src)
+      .then((res) => res.arrayBuffer())
+      .then((buffer) => Buffer.from(buffer));
+    const bufferExists = buffer.length > 0;
+    const {
+      metadata: { width, height },
+    } = bufferExists
+      ? await getPlaiceholder(buffer)
+      : {
+          metadata: { width: 0, height: 0 },
+        };
+    const url = bufferExists ? src : "";
+    return {
+      img: { url: url, width, height },
+    };
+  }
+};
+
+const metadataCache = new Map<string, Metadata>();
 
 const detectTitle = ($: CheerioAPI, url: string) => {
   let t =
@@ -11,7 +42,7 @@ const detectTitle = ($: CheerioAPI, url: string) => {
   return t;
 };
 
-const detectImage = ($: CheerioAPI, url: string) => {
+const detectImage = async ($: CheerioAPI, url: string) => {
   let tmp =
     $('meta[property="og:image"]').attr("content") ??
     $('meta[property="og:image:url"]').attr("content") ??
@@ -19,9 +50,9 @@ const detectImage = ($: CheerioAPI, url: string) => {
     $('meta[name="twitter:image"]').attr("content") ??
     $('link[rel="apple-touch-icon"]').attr("href") ??
     "";
-
   let imgUrl = tmp;
-  if (imgUrl != "" && !imgUrl.startsWith("http")) {
+
+  if (imgUrl !== "" && !imgUrl.startsWith("http")) {
     let imgurl_minus_https = url.substring(url.indexOf("/") + 2);
     if (imgurl_minus_https.match("/")) {
       imgurl_minus_https = imgurl_minus_https.substring(
@@ -32,7 +63,7 @@ const detectImage = ($: CheerioAPI, url: string) => {
     imgUrl =
       url.substring(0, url.indexOf("/")) + "//" + imgurl_minus_https + imgUrl;
   }
-  return imgUrl;
+  return (await getImageMeta(imgUrl)).img;
 };
 
 const detectDescription = ($: CheerioAPI) => {
@@ -44,25 +75,32 @@ const detectDescription = ($: CheerioAPI) => {
 };
 
 export default async function fetchMeta(url: string) {
-  const req = new Request(url, { headers: { "User-Agent": "bot" } });
-  const metaData = {
+  const cachedMetadata: Metadata | undefined = metadataCache.get(url);
+  if (cachedMetadata) {
+    return cachedMetadata;
+  }
+  const req: Request = new Request(url, { headers: { "User-Agent": "bot" } });
+  const metaDataEmpty: Metadata = {
     url: url,
     title: "",
     description: "",
     image: { url: "", width: 0, height: 0 },
   };
-  const metas = await fetch(req)
-    .then((res) => res.text())
+  const metaData: Metadata = await fetch(req)
+    .then(async (res) => res.text())
     .then(async (text) => {
       const $ = load(text);
-      metaData.title = detectTitle($, url);
-      metaData.description = detectDescription($);
-      const image = getImageSize(detectImage($, url));
-      metaData.image = (await image).img;
-      return metaData;
+      const meta = {
+        url: url,
+        title: detectTitle($, url),
+        description: detectDescription($),
+        image: await detectImage($, url),
+      };
+      metadataCache.set(url, meta);
+      return meta;
     })
     .catch((e) => {
-      return metaData;
+      return metaDataEmpty;
     });
-  return metas;
+  return metaData;
 }
