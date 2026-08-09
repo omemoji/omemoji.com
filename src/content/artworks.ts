df4b9faceb2c47ac239c5829bdf7f805df2a2028
omemoji <me@omemoji.com>
2026-08-10 03:22:35 +0900

    artwork管理方法を変更

--- src/content/artworks.ts
+++ src/content/artworks.ts
@@ -1,12 +1,12 @@
 import * as z from "zod";
 
 const artworkSchema = z.object({
-  id: z.string(),
-  src: z.string(),
   title: z.string(),
+  description: z.string().optional(),
+  date: z.string(),
+  src: z.string(),
   tags: z.array(z.string()),
   href: z.string().optional(),
-  description: z.string().optional(),
 });
 
 export type Artwork = z.infer<typeof artworkSchema>;
