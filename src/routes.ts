bc19131675022ef9ca6582f897cc69e2c9164937
omemoji <me@omemoji.com>
2026-08-11 03:46:44 +0900

    routes.tsに各記事と記事一覧ページを追加

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,45 @@
 import path from "node:path";
 
+import { type Article, loadArticles } from "@/content/articles";
+import type { Artwork } from "@/content/artworks";
 import { loadArtworks } from "@/content/artworks";
 
-type Route = {
-  path: string;
-  page: string;
+type PageProps = {
+  About: undefined;
+  ArticlesList: undefined;
+  ArtworksList: undefined;
+  ArticlePage: { article: Article };
+  ArtworkPage: { artwork: Artwork };
 };
+
+type Route = {
+  [P in keyof PageProps]: { path: string; page: P } & (PageProps[P] extends undefined
+    ? { props?: undefined }
+    : { props: PageProps[P] });
+}[keyof PageProps];
 
 const artworksRoutes: Route[] = loadArtworks(
   path.join(import.meta.dirname, "../content/artworks")
 ).map((artwork) => ({
   path: `/artworks/${artwork.id}`,
   page: "ArtworkPage",
+  props: { artwork },
+}));
+
+const articlesRoutes: Route[] = loadArticles(
+  path.join(import.meta.dirname, "../content/articles")
+).map((article) => ({
+  path: `/articles/${article.slug}`,
+  page: "ArticlePage",
+  props: { article },
 }));
 
 export const routes: Route[] = [
   { path: "/", page: "About" },
+  // 作品
   { path: "/artworks", page: "ArtworksList" },
   ...artworksRoutes,
+  // 記事
+  { path: "/articles", page: "ArticlesList" },
+  ...articlesRoutes,
 ];
