79b9151395e75b4af1769e70c3e27adb6ed96ad1
omemoji <me@omemoji.com>
2026-08-12 02:00:45 +0900

    bun check

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,5 @@
 import SNSList from "@/components/SNSList";
 
-const COMMENT_FORM =
-  "https://docs.google.com/forms/d/e/1FAIpQLSegu77jt7W0JTHs9wfRIT3aPXFIItcKb_lE_kcJlTLJLXFVeg/viewform?usp=dialog";
-
 export default function Footer() {
   return (
     <footer className="site-footer">
